5697fe88c43fb5816d9a51eba9b1ea6fd5de5a3f
dmheisel <d.m.heisel@gmail.com>
2019-08-30 16:53:52 -0500

    basic layout and imports for home page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import Home from '../Home/Home'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
-      <Router>
-        <Route exact path='/' component={Home} />
-      </Router>
-    );
+			<Router>
+				<CssBaseline />
+				<Route exact path='/' component={Home} />
+			</Router>
+		);
   }
 }
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+//material-ui imports
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import ButtonBase from '@material-ui/core/ButtonBase'
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+	root: {
+		flexGrow: 1
+	},
+	paper: {
+		// padding: theme.spacing(2),
+		margin: 'auto',
+		maxWidth: 500
+	},
+	image: {
+		width: 128,
+		height: 128
+	},
+	img: {
+		margin: 'auto',
+		display: 'block',
+		maxWidth: '100%',
+		maxHeight: '100%'
+	}
+});
 
 class Home extends Component {
   render() {
+    const {classes} = this.props
     return (
-      <div>
-        Home
+      <div className={classes.root}>
+        <Paper className={classes.Paper}>
+          <Grid container spacing={2}>
+            
+          </Grid>
+        </Paper>
       </div>
     );
   }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
