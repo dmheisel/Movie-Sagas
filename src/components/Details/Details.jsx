ce17d243b038b23897c2461874f5397854b19fb4
dmheisel <d.m.heisel@gmail.com>
2019-08-30 21:45:29 -0500

    added details page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+//material-ui imports
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import { Typography } from '@material-ui/core';
+
+const styles = theme => ({
+	root: {
+		flexGrow: 1
+	},
+	paper: {
+		padding: theme.spacing(2),
+		margin: 'auto',
+		maxWidth: '80%'
+	}
+});
 
 class Details extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    );
-  }
-}
+	render() {
+		const { classes } = this.props;
 
-export default Details;
+		return (
+			<div className={classes.root}>
+				<Paper className={classes.paper}>
+					<Typography variant='body1'>
+						{this.props.movie.description}
+					</Typography>
+					<Button
+						onClick={() => this.props.history.push('/')}
+
+						color='secondary'>
+						Return to List
+					</Button>
+					<Button
+						onClick={() => this.props.history.push('/edit')}
+						color='secondary'>
+						Edit
+					</Button>
+				</Paper>
+			</div>
+		);
+	}
+}
+const mapStateToProps = reduxStore => ({
+	movie: reduxStore.currentMovie
+});
+export default connect(mapStateToProps)(withStyles(styles)(Details));
