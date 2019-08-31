e850a9f1e82f7367782154c8be3bd372bb4c0299
dmheisel <d.m.heisel@gmail.com>
2019-08-30 21:48:41 -0500

    added grid to home to show movies, reactive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 //material-ui imports
-import Paper from '@material-ui/core/Paper'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
-import ButtonBase from '@material-ui/core/ButtonBase'
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -13,13 +13,18 @@ const styles = theme => ({
 		flexGrow: 1
 	},
 	paper: {
-		// padding: theme.spacing(2),
+		padding: theme.spacing(2),
 		margin: 'auto',
-		maxWidth: 500
+		maxWidth: '80%'
+	},
+	moviePoster: {
+		padding: theme.spacing(2),
+		margin: 'auto',
+		// width: '30vw'
 	},
 	image: {
-		width: 128,
-		height: 128
+		width: 'auto',
+		height: 250
 	},
 	img: {
 		margin: 'auto',
@@ -31,24 +36,51 @@ const styles = theme => ({
 
 class Home extends Component {
 	componentDidMount() {
-		this.props.dispatch({type: 'FETCH_MOVIES'})
+		this.props.dispatch({ type: 'FETCH_MOVIES' });
 	}
-	render() {
-    const {classes} = this.props
-    return (
-			<div className={classes.root}>
-				{JSON.stringify(this.props.movies)}
-        <Paper className={classes.Paper}>
-          <Grid container spacing={8}>
 
-          </Grid>
-        </Paper>
-      </div>
-    );
-  }
+	handleClick = (movie) => {
+		this.props.dispatch({ type: 'DISPLAY_MOVIE', payload: movie })
+		this.props.history.push('/details')
+	}
+
+	render() {
+		const { classes } = this.props;
+
+		let gridHtml = this.props.movies.map((movie, index) => {
+			return (
+				<Grid container item key={index} xs={12} md={6} lg={4}>
+					<Paper className={classes.moviePoster}>
+						<Grid item>
+							<ButtonBase
+								className={classes.image}
+								onClick={() => this.handleClick(movie)}>
+								<img className={classes.img} alt='complex' src={movie.poster} />
+							</ButtonBase>
+						</Grid>
+						<Grid item>
+							<Typography variant='body2'>
+								{movie.title}
+							</Typography>
+						</Grid>
+					</Paper>
+				</Grid>
+			);
+		});
+
+		return (
+			<div className={classes.root}>
+				<Paper className={classes.paper}>
+					<Grid container spacing={2}>
+						{gridHtml}
+					</Grid>
+				</Paper>
+			</div>
+		);
+	}
 }
 const mapStateToProps = reduxStore => ({
 	movies: reduxStore.movies
-})
+});
 
 export default connect(mapStateToProps)(withStyles(styles)(Home));
